06551968ae005235a90c73db1323cbd651757e68
Lark <lark@Larks-MBP.home>
2025-09-21 14:57:23 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.11-slim
+
+RUN apt-get update && apt-get install -y \
+    poppler-utils \
+    tesseract-ocr \
+    libgl1 \
+    libglib2.0-0 \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+COPY requirements.txt /app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . /app
+
+RUN mkdir -p /app/outputs
+
+ENV PORT 8000
+
+CMD ["bash", "-lc", "uvicorn main:app --host 0.0.0.0 --port ${PORT}"]
+
---
+++ main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import pytesseract
+import numpy as np
+import cv2
+from pdf2image import convert_from_bytes
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import io
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # only allow your React app
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+
+    contents = await file.read()
+    pages = convert_from_bytes(contents, dpi=300)
+
+    text_str = ""
+
+    for page in pages:
+
+        curr_page = np.array(page)
+        rotated = cv2.rotate(curr_page, cv2.ROTATE_90_CLOCKWISE)
+        
+        h, w, _ = rotated.shape
+        mid_x = w // 2
+        width = 300  
+
+        left_page  = rotated[:, :mid_x + width]
+        right_page = rotated[:, mid_x + width:]
+
+        text_left  = pytesseract.image_to_string(left_page, lang="eng")
+        text_right = pytesseract.image_to_string(right_page, lang="eng")
+
+        text_str += text_left + "\n" + text_right + "\n"
+
+    return JSONResponse({"text": text_str})
